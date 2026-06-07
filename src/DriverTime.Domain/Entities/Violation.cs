c7a5ba6060228e58c91d8ede13ecb4f6ceb6dbed
leszekdyja <vgyjntjfrk@privaterelay.appleid.com>
2026-06-07 10:56:39 +0200

    Create Violation.cs
    Add Violation entity

---
+++ src/DriverTime.Domain/Entities/Violation.cs
@@ -0,0 +1,24 @@
+using DriverTime.Domain.Common;
+
+namespace DriverTime.Domain.Entities;
+
+public class Violation : BaseEntity
+{
+    public Guid DriverId { get; set; }
+
+    public Driver? Driver { get; set; }
+
+    public string ViolationType { get; set; } = string.Empty;
+
+    public string RegulationReference { get; set; } = string.Empty;
+
+    public string Severity { get; set; } = string.Empty;
+
+    public int DurationMinutes { get; set; }
+
+    public DateTime ViolationStart { get; set; }
+
+    public DateTime ViolationEnd { get; set; }
+
+    public DateTime CalculatedAt { get; set; } = DateTime.UtcNow;
+}
