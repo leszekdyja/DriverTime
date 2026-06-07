c83907de4ca7fb09c97de0fb2d697ba93b9d796f
leszekdyja <vgyjntjfrk@privaterelay.appleid.com>
2026-06-07 11:06:20 +0200

    Update ApplicationDbContext.cs
    Connect ApplicationDbContext interface

--- src/DriverTime.Infrastructure/Persistence/ApplicationDbContext.cs
+++ src/DriverTime.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
+using DriverTime.Application.Common.Interfaces;
 using DriverTime.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace DriverTime.Infrastructure.Persistence;
 
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext : DbContext, IApplicationDbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
@@ -33,21 +34,13 @@ public class ApplicationDbContext : DbContext
         base.OnModelCreating(modelBuilder);
 
         modelBuilder.Entity<Company>().ToTable("companies");
-
         modelBuilder.Entity<User>().ToTable("users");
-
         modelBuilder.Entity<Driver>().ToTable("drivers");
-
         modelBuilder.Entity<Vehicle>().ToTable("vehicles");
-
         modelBuilder.Entity<DriverActivity>().ToTable("driver_activities");
-
         modelBuilder.Entity<Violation>().ToTable("violations");
-
         modelBuilder.Entity<ImportFile>().ToTable("import_files");
-
         modelBuilder.Entity<Notification>().ToTable("notifications");
-
         modelBuilder.Entity<AuditLog>().ToTable("audit_log");
     }
 }
