988f15a66f791d33a93055b781dedb278be92c3b
leszekdyja <vgyjntjfrk@privaterelay.appleid.com>
2026-06-07 11:04:25 +0200

    Create DependencyInjection.cs
    Add Infrastructure dependency injection

---
+++ src/DriverTime.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,19 @@
+using DriverTime.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DriverTime.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+        return services;
+    }
+}
