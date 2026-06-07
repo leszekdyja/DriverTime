353a7613b3fea1eb0b42c374e8844e4f1e5bd28c
leszekdyja <vgyjntjfrk@privaterelay.appleid.com>
2026-06-07 11:03:37 +0200

    Create ApplicationDbContext.cs
    Add ApplicationDbContext

---
+++ src/DriverTime.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,53 @@
+using DriverTime.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DriverTime.Infrastructure.Persistence;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Company> Companies => Set<Company>();
+
+    public DbSet<User> Users => Set<User>();
+
+    public DbSet<Driver> Drivers => Set<Driver>();
+
+    public DbSet<Vehicle> Vehicles => Set<Vehicle>();
+
+    public DbSet<DriverActivity> DriverActivities => Set<DriverActivity>();
+
+    public DbSet<Violation> Violations => Set<Violation>();
+
+    public DbSet<ImportFile> ImportFiles => Set<ImportFile>();
+
+    public DbSet<Notification> Notifications => Set<Notification>();
+
+    public DbSet<AuditLog> AuditLogs => Set<AuditLog>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Company>().ToTable("companies");
+
+        modelBuilder.Entity<User>().ToTable("users");
+
+        modelBuilder.Entity<Driver>().ToTable("drivers");
+
+        modelBuilder.Entity<Vehicle>().ToTable("vehicles");
+
+        modelBuilder.Entity<DriverActivity>().ToTable("driver_activities");
+
+        modelBuilder.Entity<Violation>().ToTable("violations");
+
+        modelBuilder.Entity<ImportFile>().ToTable("import_files");
+
+        modelBuilder.Entity<Notification>().ToTable("notifications");
+
+        modelBuilder.Entity<AuditLog>().ToTable("audit_log");
+    }
+}
