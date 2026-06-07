9f21eca912a6e7ef303f86d80198ba45e64051f4
leszekdyja <vgyjntjfrk@privaterelay.appleid.com>
2026-06-07 10:52:36 +0200

    Update Vehicle.cs
    Refactor Vehicle entity

--- src/DriverTime.Domain/Entities/Vehicle.cs
+++ src/DriverTime.Domain/Entities/Vehicle.cs
@@ -1,9 +1,9 @@
+using DriverTime.Domain.Common;
+
 namespace DriverTime.Domain.Entities;
 
-public class Vehicle
+public class Vehicle : BaseEntity
 {
-    public Guid Id { get; set; }
-
     public Guid CompanyId { get; set; }
 
     public string RegistrationNumber { get; set; } = string.Empty;
@@ -11,6 +11,4 @@ public class Vehicle
     public string Vin { get; set; } = string.Empty;
 
     public bool Active { get; set; } = true;
-
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
