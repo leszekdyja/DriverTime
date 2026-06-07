d0551f40ad77bf1d0fb963fffc94ef47ba4547f3
leszekdyja <vgyjntjfrk@privaterelay.appleid.com>
2026-06-07 10:57:16 +0200

    Create ImportFile.cs
    Add ImportFile entity

---
+++ src/DriverTime.Domain/Entities/ImportFile.cs
@@ -0,0 +1,20 @@
+using DriverTime.Domain.Common;
+
+namespace DriverTime.Domain.Entities;
+
+public class ImportFile : BaseEntity
+{
+    public Guid CompanyId { get; set; }
+
+    public Company? Company { get; set; }
+
+    public string OriginalFileName { get; set; } = string.Empty;
+
+    public string StoredFileName { get; set; } = string.Empty;
+
+    public long FileSize { get; set; }
+
+    public string Status { get; set; } = string.Empty;
+
+    public DateTime UploadedAt { get; set; } = DateTime.UtcNow;
+}
