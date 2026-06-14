7042c2cc3daa50a8ff273789c2ecf18037386a09
Leszek Dyja <dyjson@konto.pl>
2026-06-14 11:45:36 +0200

    Remove duplicated root project folders

--- DriverTime.Application/Interfaces/IDddParserGateway.cs
+++
@@ -1,8 +0,0 @@
-using DriverTime.Application.DTOs.Ddd;
-
-namespace DriverTime.Application.Interfaces;
-
-public interface IDddParserGateway
-{
-    Task<DddParseResultDto> ParseAsync(string filePath, CancellationToken cancellationToken = default);
-}
